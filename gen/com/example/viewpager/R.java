1db9ad1aa4a3aa79e012d1820a228ff7b2860933
weipingQin <995308963@qq.com>
2015-11-05 17:59:50 +0800

    modify the test.xml

--- gen/com/example/viewpager/R.java
+++ gen/com/example/viewpager/R.java
@@ -31,9 +31,12 @@ public final class R {
         public static final int e=0x7f020006;
         public static final int ic_launcher=0x7f020007;
         public static final int selector_dot=0x7f020008;
+        public static final int wheel_bg=0x7f020009;
+        public static final int wheel_val=0x7f02000a;
     }
     public static final class id {
         public static final int dot_layout=0x7f070002;
+        public static final int id_quantity=0x7f070004;
         public static final int image=0x7f070003;
         public static final int tv_intro=0x7f070001;
         public static final int viewPager=0x7f070000;
@@ -41,6 +44,7 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int adapter_ad=0x7f030001;
+        public static final int test=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f060000;
---
+++ src/com/example/viewpager/test/TestActivity.java
@@ -0,0 +1,21 @@
+package com.example.viewpager.test;
+
+import com.example.viewpager.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.test);
+		init();
+	}
+	
+	private void init(){
+		
+	}
+	
+}
